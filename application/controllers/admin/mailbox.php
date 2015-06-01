2b9201abcb8e7e731946c467b5bd65abeb33b53a
WhiteCodaInc <notifications@whitecoda.com>
2015-06-01 16:11:48 +0530

    update

--- application/controllers/admin/mailbox.php
+++ application/controllers/admin/mailbox.php
@@ -70,12 +70,11 @@ class Mailbox extends CI_Controller {
                 rsort($emails);
                 $data = array();
                 echo '<pre>';
-                $threds = imap_thread($this->stream);
-                print_r($threds);
-                die();
+//                $threds = imap_thread($this->stream);
+//                print_r($threds);
+//                die();
                 foreach ($emails as $key => $email_id) {
-                    $mailHeader = @imap_headerinfo($this->stream, $email_id);
-
+//                    $mailHeader = @imap_headerinfo($this->stream, $email_id);
 //                    $mailHeader = @imap_header($this->stream, $email_id);
 //                    print_r($mailHeader);
 //                    print_r(imap_header($this->stream, $email_id));
@@ -86,13 +85,13 @@ class Mailbox extends CI_Controller {
 //                    }
 //                    print_r($mailHeader);
 //                    echo $mailHeader;
-                    $headers = "From: {$this->inbox_user}\r\n" .
-                            "Reply-To: {$this->inbox_user}\r\n" .
-                            "MIME-Version: 1.0\r\n" .
+//                    $headers = "From: {$this->inbox_user}\r\n" .
+//                            "Reply-To: {$this->inbox_user}\r\n" .
+//                            "MIME-Version: 1.0\r\n" .
 //                            "In-Reply-To: {$mailHeader->in_reply_to}\r\n" .
 //                            "References: {$mailHeader->references}\r\n" .
 //                            "Envelope-to: vishal@mikhailkuznetsov.com\r\n" .
-                            "Content-Type: text/html; charset=ISO-8859-1\r\n";
+//                            "Content-Type: text/html; charset=ISO-8859-1\r\n";
 //                    $rely = imap_mail("vishaltesting7@gmail.com", $mailHeader->subject, "How are you?", $headers);
 //                    if ($rely)
 //                        echo 'SEND';
@@ -101,21 +100,21 @@ class Mailbox extends CI_Controller {
 //                    print_r(imap_fetchheader($this->stream, $email_id));
 //                    echo '<br>';
 //                    $data['email'][$key]['headerInfo'] = $mailHeader;
-//                    $overview = imap_fetch_overview($this->stream, $email_id, 0);
-//                    $data['email'][$key]['id'] = $overview[0]->uid;
-//                    $data['email'][$key]['subject'] = $this->decode_imap_text($overview[0]->subject);
-//                    $data['email'][$key]['from'] = $this->decode_imap_text($overview[0]->from);
-//                    $data['email'][$key]['to'] = $this->decode_imap_text($overview[0]->to);
-//                    $data['email'][$key]['date'] = date('m-d-Y H:i', strtotime($overview[0]->date));
-//                    $data['email'][$key]['status'] = ($overview[0]->seen) ? 1 : 0;
-//                    $data['email'][$key]['body'] = imap_fetchbody($this->stream, $email_id, 1);
+                    $overview = imap_fetch_overview($this->stream, $email_id, 0);
+                    $data['email'][$key]['id'] = $overview[0]->uid;
+                    $data['email'][$key]['subject'] = $this->decode_imap_text($overview[0]->subject);
+                    $data['email'][$key]['from'] = $this->decode_imap_text($overview[0]->from);
+                    $data['email'][$key]['to'] = $this->decode_imap_text($overview[0]->to);
+                    $data['email'][$key]['date'] = date('m-d-Y H:i', strtotime($overview[0]->date));
+                    $data['email'][$key]['status'] = ($overview[0]->seen) ? 1 : 0;
+                    $data['email'][$key]['body'] = imap_fetchbody($this->stream, $email_id, 1);
                 }
             } else {
                 $data['email'] = array();
             }
         }
 //        $data['folder'] = $this->getInboxFolder();
-//        print_r($data);
+        print_r($data);
         die();
         $this->load->view('admin/admin_header');
         $this->load->view('admin/admin_top');
