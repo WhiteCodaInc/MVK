2fb281b519d5394eb30589f68ba521f27e904e8f
WhiteCodaInc <notifications@whitecoda.com>
2015-06-01 16:56:12 +0530

    update

--- application/views/admin/mailbox.php
+++ application/views/admin/mailbox.php
@@ -499,7 +499,7 @@ switch ($msg) {
             $('#mail-body #from').text($('tr#' + id + ' td.name').text());
             $('#mail-body #date').text($('tr#' + id + ' td.time').text());
             $('#mail-body #to').text($('span.to' + id).text());
-            $('#mail-body #body').text($('span.body' + id).html());
+            $('#mail-body #body').text($('span.body' + id).text());
         });
 
         $('button.reply').click(function () {
