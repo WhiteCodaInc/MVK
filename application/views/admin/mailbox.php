815dec0cdafd70a4eab2a6c5b91f6eda47f2df5d
WhiteCodaInc <notifications@whitecoda.com>
2015-06-02 11:09:45 +0530

    update

--- application/views/admin/mailbox.php
+++ application/views/admin/mailbox.php
@@ -280,25 +280,57 @@
                 <h4 class="modal-title"><i class="fa fa-envelope-o"></i> View Email</h4>
             </div>
             <div class="modal-body">
-                <div class="input-group" style="width: 100%">
-                    <h4 style="float: left;padding-right: 5px;"><label>Subject :</label></h4>
-                    <h4 id="subject"></h4>
-                </div>
-                <div class="input-group" style="width: 100%">
-                    <h4 style="float: left;padding-right: 5px;"><label>From :</label></h4>
-                    <h4 id="from"></h4>
-                </div>
-                <div class="input-group" style="width: 100%">
-                    <h4 style="float: left;padding-right: 5px;"><label>Date :</label></h4>
-                    <h4 id="date"></h4>
-                </div>
-                <div class="input-group" style="width: 100%">
-                    <h4 style="float: left;padding-right: 5px;"><label>To :</label></h4>
-                    <h4 id="to"></h4>
-                </div>
-                <div class="form-group">
-                    <!--<textarea name="message" id="body" class="form-control" placeholder="Message" style="height: 120px;"></textarea>-->
-                    <div class="" id="body"></div>
+                <div class="box box-solid">
+                    <div class="box-header">
+                        <h3 class="box-title">Collapsible Accordion</h3>
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="box-group" id="accordion">
+                            <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+                            <div class="panel box box-primary">
+                                <div class="box-header">
+                                    <h4 class="box-title">
+                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="collapsed">
+                                            Collapsible Group Item #1
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse" style="height: 0px;">
+                                    <div class="box-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel box box-danger">
+                                <div class="box-header">
+                                    <h4 class="box-title">
+                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed">
+                                            Collapsible Group Danger
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse" style="height: 0px;">
+                                    <div class="box-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel box box-success">
+                                <div class="box-header">
+                                    <h4 class="box-title">
+                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="">
+                                            Collapsible Group Success
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseThree" class="panel-collapse collapse in">
+                                    <div class="box-body">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- /.box-body -->
                 </div>
             </div>
             <div class="modal-footer clearfix">
@@ -504,7 +536,7 @@ switch ($msg) {
                 success: function (data, textStatus, jqXHR) {
                     var json = JSON.parse(data);
                     $.each(json, function (i, item) {
-                        console.log(json[i].id);
+//                        console.log(json[i].id);
                     });
                 }
             });
