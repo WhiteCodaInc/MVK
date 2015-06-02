853806a41c1948dc8f79ea31df0b4150581dae12
WhiteCodaInc <notifications@whitecoda.com>
2015-06-02 12:04:18 +0530

    update

--- application/views/admin/mailbox.php
+++ application/views/admin/mailbox.php
@@ -281,13 +281,9 @@
             </div>
             <div class="modal-body">
                 <div class="box box-solid">
-                    <div class="box-header">
-                        <h3 class="box-title">Collapsible Accordion</h3>
-                    </div><!-- /.box-header -->
                     <div class="box-body">
                         <div class="box-group" id="accordion">
-                            <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-                            <div class="panel box box-primary">
+                            <div class="panel box box-primary demo" style="display: none">
                                 <div class="box-header">
                                     <h4 class="box-title">
                                         <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="collapsed">
@@ -297,35 +293,7 @@
                                 </div>
                                 <div id="collapseOne" class="panel-collapse collapse" style="height: 0px;">
                                     <div class="box-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel box box-danger">
-                                <div class="box-header">
-                                    <h4 class="box-title">
-                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed">
-                                            Collapsible Group Danger
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseTwo" class="panel-collapse collapse" style="height: 0px;">
-                                    <div class="box-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel box box-success">
-                                <div class="box-header">
-                                    <h4 class="box-title">
-                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="">
-                                            Collapsible Group Success
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseThree" class="panel-collapse collapse in">
-                                    <div class="box-body">
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
                                     </div>
                                 </div>
                             </div>
@@ -536,7 +504,14 @@ switch ($msg) {
                 success: function (data, textStatus, jqXHR) {
                     var json = JSON.parse(data);
                     $.each(json, function (i, item) {
-//                        console.log(json[i].id);
+                        $acordian = $('#accordion .demo').clone();
+                        $acordian.removeClass('demo');
+                        $acordian.removeProp('style');
+                        $acordian.find('h4 > a').prop('href', "#collapse" + item.id);
+                        $acordian.find('#collapseOne .box-body').text(item.body);
+                        $acordian.find('h4 > a').text(item.from);
+                        $acordian.find('#collapseOne').prop('id', "collapse" + item.id);
+                        $('#accordion .demo').before($acordian);
                     });
                 }
             });
