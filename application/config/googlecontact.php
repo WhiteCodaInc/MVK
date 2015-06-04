fa9ce6ac3715a1ead27807029611cad98bed255c
WhiteCodaInc <notifications@whitecoda.com>
2015-06-04 15:42:53 +0530

    update

--- application/config/googlecontact.php
+++ application/config/googlecontact.php
@@ -3,6 +3,6 @@
 $config['googlecontact']['application_name'] = 'MVK-Contact';
 $config['googlecontact']['client_id'] = '25105222102-i9v1vm6svak9t953o8k9ntqnjlk3relb.apps.googleusercontent.com';
 $config['googlecontact']['client_secret'] = '0fSaaeC1tp2PaBHHF7d1-fLR';
-$config['googlecontact']['redirect_uri'] = 'http://mikhailkuznetsov.com/mikhail/import/contacts';
+$config['googlecontact']['redirect_uri'] = 'http://mikhailkuznetsov.com/admin/import/contacts';
 $config['googlecontact']['api_key'] = 'AIzaSyApF2HSWkGVRSjf98S9aHhl7mxjVgi2tSY';
 
