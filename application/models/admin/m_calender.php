5e9053112db9ac6cd93933cd48c3b1faa36a3f9b
WhiteCodaInc <notifications@whitecoda.com>
2015-06-04 17:53:34 +0530

    update

--- application/models/admin/m_calender.php
+++ application/models/admin/m_calender.php
@@ -227,6 +227,7 @@ class M_calender extends CI_Model {
                     $set['occurance'] = NULL;
                     $set['end_type'] = NULL;
                 }
+                die("RES[IS_REPEAT] : 0 && RES[IS_REPEAT] == NULL");
             } else if ($res['is_repeat'] == 1 && ($set['is_repeat'] == 0 || $set['end_type'] == "never")) {
                 $this->db->delete('schedule', array('refer_id' => $eid));
                 $set['occurance'] = NULL;
