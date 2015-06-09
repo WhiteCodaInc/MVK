2b3de438d012f06dacc0717dccfdf69e02ee306e
WhiteCodaInc <notifications@whitecoda.com>
2015-06-09 11:59:41 +0530

    update

--- application/libraries/common.php
+++ application/libraries/common.php
@@ -164,6 +164,9 @@ class Common {
     function sendSMS($to, $body) {
         $adminInfo = $this->getAdminInfo();
         try {
+            echo '<pre>';
+            print_r($adminInfo);
+            die();
             $msg = $this->_CI->twilio->account->messages->create(
                     array(
                         'To' => $to,
