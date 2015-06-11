87d9c5fccb48c1bd9f89bb8fbf65dc2e01251897
WhiteCodaInc <notifications@whitecoda.com>
2015-06-11 11:11:52 +0530

    update

--- application/views/admin/contact-detail.php
+++ application/views/admin/contact-detail.php
@@ -2,8 +2,11 @@
     #contact-data-table tr td,#contact-data-table tr th{
         text-align: center;
     }
-    .dataTables_filter {
-        display: none;
+    /*    .dataTables_filter {
+            display: none;
+        }*/
+    .dataTables_wrapper:first-child{
+        display: none
     }
 </style>
 <!-- Right side column. Contains the navbar and content of the page -->
@@ -151,23 +154,8 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="box" >
-                    <div class="box-header">
-                        <h3 class="box-title">Contact Detail</h3>
-                    </div><!-- /.box-header -->
-                    <div class="row">
-                        <div class="col-md-12" style="margin-left: 10px">
-                            <!--<a href="<?= site_url() ?>admin/contacts/addContact" class="btn btn-success btn-sm create">
-                                <i class="fa fa-plus"></i>
-                                Create New Contact
-                            </a>
-                            <button style="margin-left: 10px" value="Delete" class="btn btn-danger btn-sm delete" id="Delete" type="button" >Delete</button>
-                            -->
-                        </div>
-                    </div>
-
                     <form name="checkForm" id="checkForm" action="" method="post">
                         <div class="box-body table-responsive" id="data-panel">
-
                             <table id="contact-data-table" class="table table-bordered table-striped">
                                 <thead>
                                     <tr>
