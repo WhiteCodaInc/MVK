2390575718a1223f3fb2306d09050360ad8d5cdc
WhiteCodaInc <notifications@whitecoda.com>
2015-06-17 17:10:12 +0530

    update

--- application/views/admin/email-notify.php
+++ application/views/admin/email-notify.php
@@ -1,27 +1,26 @@
-<?= print_r($emails) ?>
-<!--<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">
     <i class="fa fa-envelope-o"></i>
     <span class="label label-success ebadge"><?= count($emails) ?></span>
 </a>
 <ul class="dropdown-menu">
     <li class="header">You have <span class="totalC"><?= count($emails) ?></span> comments</li>
     <li>
-         inner menu: contains the actual data 
+        <!-- inner menu: contains the actual data -->
         <ul class="menu unreadSMS">
-<?php foreach ($emails as $email) { ?>
-                    <li> start message 
-                        <a href="<?= site_url() . 'admin/mailbox/login/' . $email->id ?>">
-    <?php $arr = explode("/", $string, 2); ?>
-                            <h4 style="margin: 0">
-    <?= $arr[0] ?>
-                                <small><i class="fa fa-clock-o"></i><?= $email->date ?></small>
-                            </h4>
-                            <p style="margin: 0"><?= $email->subject ?></p>
-                        </a>
-                    </li> end message 
-<?php } ?>
+            <?php foreach ($emails as $email) { ?>
+                <li><!-- start message -->
+                    <a href="<?= site_url() . 'admin/mailbox/login/' . $email->id ?>">
+                        <?php $arr = explode("/", $string, 2); ?>
+                        <h4 style="margin: 0">
+                            <?= $arr[0] ?>
+                            <small><i class="fa fa-clock-o"></i><?= $email->date ?></small>
+                        </h4>
+                        <p style="margin: 0"><?= $email->subject ?></p>
+                    </a>
+                </li><!-- end message -->
+            <?php } ?>
         </ul>
     </li>
-    <li class="footer"><a href="<?= site_url() . 'admin/comment' ?>">See All Comments</a></li>
-</ul>-->
+    <li class="footer"><a href="#">See All Comments</a></li>
+</ul>
 
