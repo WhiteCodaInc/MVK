86fee1703cd07ef79a2f9e7c12a7be975657213f
WhiteCodaInc <notifications@whitecoda.com>
2015-06-17 13:15:04 +0530

    update

--- application/views/admin/admin_top.php
+++ application/views/admin/admin_top.php
@@ -111,7 +111,7 @@
                                             </div>
                                             <h4>
                                                 <?= $comment->name ?>
-                                                <small><i class="fa fa-clock-o"></i><?= $comment->date ?></small>
+                                                <small><i class="fa fa-clock-o"></i><?= date('m-d-Y H:i', strtotime($comment->date)) ?></small>
                                             </h4>
                                             <p><?= substr($comment->comment, 0, 50); ?></p>
                                         </a>
