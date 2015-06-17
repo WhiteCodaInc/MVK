ad81dc7418e22b3e454fb9fc1791f205a54438f2
WhiteCodaInc <notifications@whitecoda.com>
2015-06-17 17:04:03 +0530

    update

--- application/views/admin/admin_top.php
+++ application/views/admin/admin_top.php
@@ -125,50 +125,20 @@
                 </li>
                 <!-- End Blog Comments -->
                 <!-- Emails -->
-                <li class="dropdown messages-menu emails">
+                <li class="dropdown messages-menu email-notify">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-envelope-o"></i>
                         <span class="label label-success ebadge">0</span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li class="header">You have <span class="totalC"><?= $this->common->getTotalUnreadComment() ?></span> comments</li>
+                        <li class="header">You have 0 comments</li>
                         <li>
                             <!-- inner menu: contains the actual data -->
-                            <ul class="menu unreadSMS">
-                                <?php foreach ($comments as $comment) { ?>
-                                    <?php
-                                    if ($comment->user_id != NULL) {
-                                        if ($comment->type == "customer") {
-                                            $cInfo = $this->common->getCustomerInfo($comment->user_id);
-                                            $img_src = ($comment->customer_avatar != "") ?
-                                                    "http://mikhailkuznetsov.s3.amazonaws.com/" . $comment->customer_avatar :
-                                                    base_url() . 'assets/admin/img/default-avatar.png';
-                                        } else {
-                                            $aInfo = $this->common->getAffiliateInfo($comment->user_id);
-                                            $img_src = ($comment->affiliate_avatar != "") ?
-                                                    "http://mikhailkuznetsov.s3.amazonawsF.com/" . $comment->affiliate_avatar :
-                                                    base_url() . 'assets/admin/img/default-avatar.png';
-                                        }
-                                    } else {
-                                        $img_src = base_url() . 'assets/admin/img/default-avatar.png';
-                                    }
-                                    ?>
-                                    <li><!-- start message -->
-                                        <a href="<?= site_url() . 'admin/comment' ?>">
-                                            <div class="pull-left">
-                                                <img style="width:60px;height:60px" src="<?= $img_src ?>" class="img-circle" alt="Customer Image"/>
-                                            </div>
-                                            <h4>
-                                                <?= $comment->name ?>
-                                                <small><i class="fa fa-clock-o"></i><?= date('m-d-Y H:i', strtotime($comment->date)) ?></small>
-                                            </h4>
-                                            <p><?= substr($comment->comment, 0, 50); ?>...</p>
-                                        </a>
-                                    </li><!-- end message -->
-                                <?php } ?>
+                            <ul class="menu">
+
                             </ul>
                         </li>
-                        <li class="footer"><a href="<?= site_url() . 'admin/comment' ?>">See All Comments</a></li>
+                        <li class="footer"><a href="#">See All Comments</a></li>
                     </ul>
                 </li>
                 <!-- ENd Email -->
@@ -207,7 +177,13 @@
 <script>
     $(document).ready(function () {
         $(function () {
-            $.post("<?= site_url() ?>admin/cpanel/getTotalUnreadEmail");
+            $.ajax({
+                type: 'POST',
+                url: "<?= site_url() ?>admin/cpanel/getTotalUnreadEmail",
+                success: function (data, textStatus, jqXHR) {
+                    $('.email-notify').html(data);
+                }
+            });
         });
         $('ul.unreadSMS > li > a').on('click', function () {
             var id = $(this).attr('id');
--- application/views/admin/email-notify.php
+++ application/views/admin/email-notify.php
@@ -1,45 +1,26 @@
 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
     <i class="fa fa-envelope-o"></i>
-    <span class="label label-success ebadge">0</span>
+    <span class="label label-success ebadge"><?= count($emails) ?></span>
 </a>
 <ul class="dropdown-menu">
-    <li class="header">You have <span class="totalC"><?= $this->common->getTotalUnreadComment() ?></span> comments</li>
+    <li class="header">You have <span class="totalC"><?= count($emails) ?></span> comments</li>
     <li>
         <!-- inner menu: contains the actual data -->
         <ul class="menu unreadSMS">
-            <?php foreach ($comments as $comment) { ?>
-                <?php
-                if ($comment->user_id != NULL) {
-                    if ($comment->type == "customer") {
-                        $cInfo = $this->common->getCustomerInfo($comment->user_id);
-                        $img_src = ($comment->customer_avatar != "") ?
-                                "http://mikhailkuznetsov.s3.amazonaws.com/" . $comment->customer_avatar :
-                                base_url() . 'assets/admin/img/default-avatar.png';
-                    } else {
-                        $aInfo = $this->common->getAffiliateInfo($comment->user_id);
-                        $img_src = ($comment->affiliate_avatar != "") ?
-                                "http://mikhailkuznetsov.s3.amazonawsF.com/" . $comment->affiliate_avatar :
-                                base_url() . 'assets/admin/img/default-avatar.png';
-                    }
-                } else {
-                    $img_src = base_url() . 'assets/admin/img/default-avatar.png';
-                }
-                ?>
+            <?php foreach ($emails as $email) { ?>
                 <li><!-- start message -->
-                    <a href="<?= site_url() . 'admin/comment' ?>">
-                        <div class="pull-left">
-                            <img style="width:60px;height:60px" src="<?= $img_src ?>" class="img-circle" alt="Customer Image"/>
-                        </div>
-                        <h4>
-                            <?= $comment->name ?>
-                            <small><i class="fa fa-clock-o"></i><?= date('m-d-Y H:i', strtotime($comment->date)) ?></small>
+                    <a href="<?= site_url() . 'admin/mailbox/login/' . $email->id ?>">
+                        <?php $arr = explode("/", $string, 2); ?>
+                        <h4 style="margin: 0">
+                            <?= $arr[0] ?>
+                            <small><i class="fa fa-clock-o"></i><?= $email->date ?></small>
                         </h4>
-                        <p><?= substr($comment->comment, 0, 50); ?>...</p>
+                        <p style="margin: 0"><?= $email->subject ?></p>
                     </a>
                 </li><!-- end message -->
             <?php } ?>
         </ul>
     </li>
-    <li class="footer"><a href="<?= site_url() . 'admin/comment' ?>">See All Comments</a></li>
+<!--    <li class="footer"><a href="<?= site_url() . 'admin/comment' ?>">See All Comments</a></li>-->
 </ul>
 
