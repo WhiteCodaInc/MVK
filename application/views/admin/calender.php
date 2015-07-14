5082dc8ec0bad0572d8710b74b1999bd10b43cd5
WhiteCodaInc <notifications@whitecoda.com>
2015-07-14 18:02:28 +0530

    update

--- application/views/admin/calender.php
+++ application/views/admin/calender.php
@@ -928,7 +928,7 @@
     function chooseContact() {
         $('div.choose input:text').focusout(function () {
             var form = $(this).parents('form').prop('id');
-            console.log(form);
+            
             var event_type = $('#' + form + ' input[name="event_type"]:checked').val();
             var user = $('#users').val().trim();
             if (user != "") {
@@ -942,6 +942,7 @@
                             "Can not Email this user because no email address is assigned!";
                     $('.msgChoose').text($msg);
                 } else {
+                    console.log(form);
                     ('#' + form + ' input[name="notify"]:nth(0)').prop('disabled', false);
 
                     var con = user.split('||');
