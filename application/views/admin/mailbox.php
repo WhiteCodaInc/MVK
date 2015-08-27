bf83260f1d2bb3beb6826f911af5175091a8f174
WhiteCodaInc <notifications@whitecoda.com>
2015-08-27 13:41:57 +0530

    update

--- application/views/admin/mailbox.php
+++ application/views/admin/mailbox.php
@@ -545,7 +545,7 @@ switch ($msg) {
                         $acordian.find('#collapseOne').prop('id', "collapse" + item.id);
                         $acordian.find('#time').text(item.date);
                         $('#accordion .demo').before($acordian);
-                        if (i == json.length - 1)
+//                        if (i == json.length - 1)
                             $("collapse" + item.id).collapse('show');
                     });
                 }
